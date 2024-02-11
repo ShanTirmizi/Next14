e921893918b593e3e9e6a2327451f9d4b558a475
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2024-02-11 07:30:09 +0000

    Added auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from '@clerk/nextjs';
+
 export default function Home() {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <h1>Hello World</h1>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
